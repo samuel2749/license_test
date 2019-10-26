0ae66bfcce9dce09758ad207902e7babf8b43bb5
Samuel <samuel2749@gmail.com.tw>
2019-10-27 01:34:28 +0800

    首頁完成。

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,32 @@
   .container#app
     ul.nav.nav-pills.justify-content-center.sticky-top
       li.nav-item
-        router-link.nav-link.active(to="./") Home
+        router-link.nav-link(to="./" :class="{active: $route.name === 'home'}") Home
       li.nav-item
-        router-link.nav-link(to="./erp") ERP 考題
+        router-link.nav-link(to="./erp" :class="{active: $route.name === 'erp'}") ERP 考題
       li.nav-item
-        router-link.nav-link(to="./pm") 專案管理 考題
-    router-view.container-fluid
+        router-link.nav-link(to="./pm" :class="{active: $route.name === 'pm'}") 專案管理 考題
+    router-view
 </template>
+<script>
+export default {
+  data () {
+    return {
 
+    }
+  },
+  mounted () {
+    console.log(this.$route)
+  }
+}
+</script>
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
 @import "@/scss/_function.scss";
 #app {
   // max-width: wh(1080);
+  .nav {
+    margin-bottom: 5vw;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
-  .home
-    .btn.btn-primary 測試
+  .home.container
+    h1.text-center 資四甲 證照測驗
+    router-link.btn.btn-lg.btn-block.btn-success(to="./erp") ERP 測驗
+    router-link.btn.btn-lg.btn-block.btn-success(to="./pm") PM 測驗
 </template>
 <script>
 // @ is an alias to /src
@@ -19,4 +21,9 @@ export default {
 }
 </script>
 <style lang="scss">
+  .home{
+    h1{
+      margin-bottom: 6vw
+    }
+  }
 </style>
